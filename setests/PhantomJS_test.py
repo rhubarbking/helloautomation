62673e07a5ffff3825f60f055a0a1f769b0f66d7
rhubarbking <rhubarb_king@hotmail.com>
2015-08-31 16:53:56 +1000

    More phantomjs stuff...

--- setests/PhantomJS_test.py
+++ setests/PhantomJS_test.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
 import os
 
-phantomjs_path = "C:\Selenium\phantomjs-2.0.0-windows\bin\phantomjs.exe"
+driver = webdriver.PhantomJS("phantomjs.exe")
+driver.set_window_size(1400, 1000)
 
-browser = webdriver.PhantomJS(executable_path=phantomjs_path, service_log_path=os.path.devnull)
-browser.set_window_size(1400, 1000)
+driver.get("http://stackoverflow.com/")
 
-browser.get("http://stackoverflow.com/")
-
-print browser.title
+print driver.title
